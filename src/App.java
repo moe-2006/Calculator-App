aa124f91da7ce543318afe36060c0fbbbffba6c8
moe-2006 <thatonekidmoe@outlook.com>
2025-01-06 02:40:27 -0700

    get committed

--- src/App.java
+++ src/App.java
@@ -1,4 +1,5 @@
-import javax.swing.JFrame;
+import java.awt.*;
+import javax.swing.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -6,10 +7,18 @@ public class App {
         System.out.println(maths.addnumber(2, 4));
 
         JFrame frame = new JFrame("Calculator");
+        JLabel label = new JLabel("idk what this is yet");
+        JPanel panel = new JPanel();
+        JButton button = new JButton();
+        panel.add(button);
+        panel.add(label);
+        frame.add(panel);
 
         frame.setSize(700, 700);
+        frame.setLocation(610,190);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
-
+        frame.setResizable(false);
+        frame.getContentPane().setBackground(Color.BLACK);
     }
 }
